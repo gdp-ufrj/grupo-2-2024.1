d44d4a9a03ebc487e5d48a631c01b61a40a9f1ea
Rafahgol <rafalima@ufrj.br>
2024-05-05 20:56:26 -0300

    deleting scenes, changing node path in timer

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -1,9 +1,9 @@
 extends Node2D
-@onready var timer: Timer = $Timer
-@onready var progress_bar: ProgressBar = $ProgressBar
-@onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var area_2d: Area2D = $Area2D
-@onready var glow = $CPUParticles2D
+@onready var timer: Timer = $Player/Timer
+@onready var progress_bar: ProgressBar = $Player/Mana
+@onready var sprite_2d: Sprite2D = $Player/Sprite2D
+@onready var area_2d: Area2D = $Player/Area2D
+@onready var glow = $Player/SkillTimeParticles
 
 var bonus_dmg: bool = false
 var qte_allowed: bool = false
@@ -26,7 +26,6 @@ func _on_timer_timeout():
 	progress_bar.value=0
 	sprite_2d.flip_v = false
 	glow.finished
-	#glow.visible = false
 	if bonus_dmg == true:
 		print("Bonus damage!")
 		bonus_dmg = false
@@ -39,7 +38,6 @@ func _quick_time_event():
 		sprite_2d.flip_v = true
 		area_2d.visible = true
 		qte_allowed = true
-		#glow.visible = true
 		glow.emitting = true
 
 func _unhandled_input(event):
@@ -52,5 +50,4 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 	mouse_over = true
 
 func _on_area_2d_mouse_exited():
-	print("leaving")
 	mouse_over = false

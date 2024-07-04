824ea937920dfcd177b39bf5966db4d289b7c430
Jvbarretods <jvbarretods@gmail.com>
2024-07-03 23:29:27 -0300

    6 levels

--- saintholyscripts/ArenaManager.gd
+++ saintholyscripts/ArenaManager.gd
@@ -2,8 +2,6 @@ extends Node2D
 
 const BALLOON = preload("res://dialogos/balloon.tscn")
 
-@onready var proxima_fase = $ProximaFase
-
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "start"
 
@@ -11,7 +9,3 @@ func _ready():
 	var dialogo: Node = BALLOON.instantiate()
 	get_tree().current_scene.add_child(dialogo)
 	dialogo.start(dialogue_resource, dialogue_start)
-
-func _process(delta):
-	if Global.combat_ended:
-		proxima_fase.show()

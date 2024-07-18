cb50eb2d6093f3f1f786c5d2c5ef79129dedd19d
Rafael Lima <95830415+rafahgol@users.noreply.github.com>
2024-07-17 22:36:51 -0300

    credits_working

--- saintholyscripts/credits.gd
+++ saintholyscripts/credits.gd
@@ -1,17 +1,46 @@
 extends Control
-@onready var next_page = $NextPage
+@onready var page_1 = $Page1
+@onready var page_2 = $Page2
+@onready var next_button = $NextButton
+@onready var back_button = $BackButton
+
+
 var standart_color
 var another_color
+var page :=0
+var credits_read:= false
+
 
 func _ready():
-	standart_color = next_page.get_theme_color("font_outline_color")
+	page = 1
 
-
-func _on_next_page_button_up():
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 
 
 func _on_timer_timeout():
-	next_page.remove_theme_color_override("font_outline_color")
-	next_page.add_theme_color_override("font_outline_color", standart_color)
 	print("alo")
+
+
+func _on_next_button_pressed():
+	if page == 1:
+		show_page2()
+		back_button.visible = true
+		credits_read = true
+	elif page == 2:
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+
+func _on_back_button_pressed():
+	if page == 2:
+		show_page1()
+	elif page == 1:
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func show_page1():
+	page_1.visible = true
+	page_2.visible = false
+	page = 1
+	
+func show_page2():
+	page_1.visible = false
+	page_2.visible = true
+	page = 2
